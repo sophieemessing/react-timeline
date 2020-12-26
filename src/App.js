521bfd695eeda12a87d154f1bf85aeefdb9199cc
Sophie Messing <sophie.e.messing@gmail.com>
2020-12-26 13:46:12 -0800

    props in TimelineEvent, wave 1

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
+
+export const testEvent = {
+    'person': 'Adele Goldberg',
+    'status': 'In Smalltalk, everything happens somewhere else.',
+    'timeStamp': '2018-05-18T22:12:03Z'
+}
+
 
 function App() {
   console.log(timelineData);
 
   // Customize the code below
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Application title</h1>
       </header>
-      <main className="App-main">
+      <main className='App-main'>
+      <TimelineEvent
+        person={testEvent.person}
+        status={testEvent.status}
+        timeStamp={testEvent.timeStamp} />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+// import testEvent from '../App'
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return (
+  <div>
+      <p>{props.person}</p>
+      <p>{props.status}</p>
+      <p>{props.timeStamp}</p>
+  </div>
+  )
 }
 
 export default TimelineEvent;
